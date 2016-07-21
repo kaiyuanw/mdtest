f5a8a8ec90bc39bc9529c1936ecf11302e56c488
Kaiyuan Wang <kaiyuanw@google.com>
2016-07-21 16:49:55 -0700

    Handle Jason's comments
    Change-Id: I4b7e7288fb4bab0127236aa0b695bc49731cffb7

--- mdtest/lib/src/commands/run.dart
+++ mdtest/lib/src/commands/run.dart
@@ -37,7 +37,6 @@ class RunCommand extends MDTestCommand {
     }
 
     this._devices = await getDevices();
-    print(_devices);
     if (_devices.isEmpty) {
       printError('No device found.');
       return 1;
